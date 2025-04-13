4788192c0d39ac710657279dfca1efd7eef8e55b
hyu4 <yuheidi9@gmail.com>
2025-04-13 15:53:43 -0700

    Add MessageCenter component

--- react-bruinmart/src/App.jsx
+++ react-bruinmart/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from 'react';
-import MessageCenter from './component/MessageCenter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,8 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      {/* Your new Message Center component */}
-<MessageCenter />
+  
+      {/* 👇 New Message Center goes here */}
+      <MessageCenter />
     </>
   )
 }
---
+++ src/component/MessageCenter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MessageCenter = () => {
+  return (
+    <div>
+      <h1>📬 Message Center</h1>
+      <p>This is where user messages will appear!</p>
+    </div>
+  );
+};
+
+export default MessageCenter;
