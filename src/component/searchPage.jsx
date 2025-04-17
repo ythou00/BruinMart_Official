e318e984ee182c06618bd95bda8b565fe9e2bc68
ScoobDaloob <paulkang409@gmail.com>
2025-04-16 19:50:06 -0700

    I added 9 boxes

--- src/component/searchPage.jsx
+++ src/component/searchPage.jsx
@@ -7,7 +7,7 @@ export default function searchPage(){
             <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '300px', marginLeft: '1750px'}}></div>
             <h1 style={{ textAlign: 'center' }}>Popular Collection</h1>
 
-      
+            <h1>Hello</h1>  {/* make sure you delete after */}
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
@@ -19,6 +19,7 @@ export default function searchPage(){
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '1235px'}}></div>
          
         </div>
+        
 
 }
 
