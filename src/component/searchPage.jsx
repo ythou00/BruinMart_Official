a5592b7abda3f96c95d4c90302cc787165b8c061
Paul Kang <paulkang@wifi-131-179-19-206.host.ucla.edu>
2025-04-16 19:45:12 -0700

    i added 9 boxes

---
+++ src/component/searchPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './searchPage.css'
+
+export default function searchPage(){
+ return  <div style={{ backgroundColor: 'white', minHeight: '100vh', color: 'black' }}>
+      
+            <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '300px', marginLeft: '1750px'}}></div>
+            <h1 style={{ textAlign: 'center' }}>Popular Collection</h1>
+
+      
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '-1300px', marginLeft: '720px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '720px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '720px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '-1295px', marginLeft: '1235px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '1235px'}}></div>
+            <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '1235px'}}></div>
+         
+        </div>
+
+}
+
