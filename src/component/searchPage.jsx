b0a0d46b25c61fc03b04b74ab50787228f9d2d48
ScoobDaloob <paulkang409@gmail.com>
2025-04-16 19:57:55 -0700

    i changed the file bane to searchpage

--- src/component/searchPage.jsx
+++ src/component/searchPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './searchPage.css'
+import './searchpage.css'
 
 export default function searchPage(){
  return  <div style={{ backgroundColor: 'white', minHeight: '100vh', color: 'black' }}>
@@ -7,7 +7,7 @@ export default function searchPage(){
             <div style={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '300px', marginLeft: '1750px'}}></div>
             <h1 style={{ textAlign: 'center' }}>Popular Collection</h1>
 
-            <h1>Hello</h1>  {/* make sure you delete after */}
+            {/* <h1>Hello</h1>  make sure you delete after */}
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
             <div style={{ width: '300px', height: '300px', backgroundColor: 'tan',  marginTop: '200px', marginLeft: '200px'}}></div>
