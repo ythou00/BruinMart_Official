43f7cfd33f4e90d1e14fd8e39555595dc0dc7c2d
YT <yt021h@gmail.com>
2025-05-08 14:04:37 -0700

    finialized the footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -8,17 +8,17 @@ export default function Footer() {
       <div className="footer-container">
         <div className="footer-left">
           <img src={logoImage} alt="BruinMart logo" className="footer-logo" />
-          <h3>BruinMart</h3>
+          <h2 className='logo-title'>BruinMart</h2>
         </div>
         <div className="footer-right">
-          <h4>Subscribe to our newsletter</h4>
-          <p>Join our community to get weekly updates and unique gifts every Friday</p>
+          <h3 className='subtitle'>Subscribe to our newsletter</h3>
+          <p className='email-detail'>Join our community to get weekly updates and new sales alert!</p>
           <input type="email" placeholder="Email address" className="email-input" />
           <button className="subscribe-button">Subscribe</button>
         </div>
       </div>
       <p className="footer-credit">
-        ©2025 NAME 1 | Name 2 | Name 3 | Name 4 | All Rights Reserved
+        ©2025 Yu-Ting Hou | Heidi Yu | Paul Kang | Isaac Lee | All Rights Reserved
       </p>
     </section>
   );
