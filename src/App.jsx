ec65617f012eb0678a142ba99954da57b678b3b6
Isaac Lee <isaaclee2006@ucla.edu>
2025-05-11 15:38:18 -0700

    Revamped user creation UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import SearchPage from './component/searchPage.jsx'
 import SellGuide from './component/sellGuide.jsx'
 import LoginPage from './component/page1.jsx'
 import AboutPage from './component/about.jsx'
-
+import CreateUser from './component/CreateUser.jsx'
+import SelectInterests from './component/SelectInterests.jsx'
+import FinishUser from './component/FinishUser.jsx'
 
 function App() {
   return (
@@ -27,6 +29,9 @@ function App() {
           <Route path="/sell-guide" element={<SellGuide />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/about-us" element={<AboutPage />} />
+          <Route path = "/CreateUser" element = {<CreateUser/>}/>
+          <Route path = "/SelectInterests" element = {<SelectInterests/>}/>
+          <Route path = "/FinishUser" element = {<FinishUser/>}/>
         </Routes>
       </div>
       
