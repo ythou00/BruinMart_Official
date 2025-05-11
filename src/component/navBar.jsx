42f01cdc97860e6f2a7fea37f035eba544dee6c3
hyu4 <yuheidi9@gmail.com>
2025-05-11 10:25:53 -0700

    Add hero section, item cards, and updated assets

--- src/component/navBar.jsx
+++ src/component/navBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './navBar.css'
+import SearchBar from "./searchBar"
+import SearchData from './data.json'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 
 export default function NavBar() {
@@ -9,14 +11,16 @@ export default function NavBar() {
         </div>
         <div className="center">
             <ul>
-                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/Home">Home</Link></li>
                 <li className="active"><Link to="/SearchPage">Search Page</Link></li>
                 <li><Link to="/MessageCenter">Message Center</Link></li>
-                <li><Link to="/saveList">Save List</Link></li>
+                <li><Link to="/SaveList">Save List</Link></li>
             </ul>
         </div>
         <div className="right">
-
+            {/*
+            <SearchBar placeholder="What are you looking for?" data={SearchData}/>
+            */}
         </div>
         
     </nav>
