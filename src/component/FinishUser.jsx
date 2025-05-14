3fcdad11dedfcc09e2d0d48e5bacaa01faa97557
YT <yt021h@gmail.com>
2025-05-13 23:23:18 -0700

    added background image and sources

--- src/component/FinishUser.jsx
+++ src/component/FinishUser.jsx
@@ -5,11 +5,23 @@ function FinishUser() {
   const navigate = useNavigate();
 
   return (
-    <div className="centered-container">
-      <div className="congrats">Congrats! 🎉</div>
-      <div className="caption">You've successfully created your account!</div>
-      <div className="bear">🧸</div>
-      <button className="done" onClick={() => navigate("/home")}>Done!</button>
+    <div className="finish-user-page">
+      <div className="centered-container">
+        <div className="congrats">Congrats! 🎉</div>
+        <div className="caption">You've successfully created your account!</div>
+        <div className="bear">🧸</div>
+        <button className="done" onClick={() => navigate("/home")}>Done!</button>
+      </div>
+      <div className="image-credit-finish">
+          Image Credit:{' '}
+          <a
+            href="https://studios.com/ucla-weyburn-terrace-paseo-and-commons-apartments.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Studios
+          </a>
+      </div>
     </div>
   )
 };
